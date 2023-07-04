34d122590ef243141e983e4b344724c373a4fe4c
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-07-04 15:46:41 +0700

    Add files via upload

---
+++ client/src/configs/theme.configs.js
@@ -0,0 +1,50 @@
+import { createTheme } from "@mui/material/styles";
+import { colors } from "@mui/material";
+
+export const themeModes = {
+  dark: "dark",
+  light: "light"
+};
+
+const themeConfigs = {
+  custom: ({ mode }) => {
+    const customPalette = mode === themeModes.dark ? {
+      primary: {
+        main: "#ff0000",
+        contrastText: "#ffffff"
+      },
+      secondary: {
+        main: "#865DFF",
+        contrastText: "#ffffff"
+      },
+      background: {
+        default: "#000000",
+        paper: "#131313"
+      }
+    } : {
+      primary: {
+        main: "#EA5455"
+      },
+      secondary: {
+        main: "#865DFF"
+      },
+      background: {
+        default: colors.white["100"],
+      }
+    };
+
+    return createTheme({
+      palette: {
+        mode,
+        ...customPalette
+      },
+      components: {
+        MuiButton: {
+          defaultProps: { disableElevation: true }
+        }
+      }
+    });
+  }
+};
+
+export default themeConfigs;
